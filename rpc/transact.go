d9d0c6a4456560d46a89d62cd91d7733949ad12d
shenpeng <shenpeng69@163.com>
2018-06-05 22:30:10 +0800

    拼写错误

--- rpc/transact.go
+++ rpc/transact.go
@@ -15,13 +15,13 @@ var (
 	defaultBaseRate = float64(100000)
 )
 
-func (s *ApiService) SubmitTranstraction(ctx context.Context, req *rpcpb.SubmitTranstractionRequest) (*rpcpb.SubmitTranstractionResponse, error) {
+func (s *ApiService) SubmitTransaction(ctx context.Context, req *rpcpb.SubmitTransactionRequest) (*rpcpb.SubmitTransactionResponse, error) {
 	rawTx := new(types.Tx)
 	if err := rawTx.UnmarshalText([]byte(req.RawTranstraction)); err != nil {
-		return nil, fmt.Errorf("submit-transtraction: %v", err.Error())
+		return nil, fmt.Errorf("submit-transaction: %v", err.Error())
 	}
 	if err := txbuilder.FinalizeTx(ctx, s.chain, rawTx); err != nil {
-		return nil, fmt.Errorf("submit-transtraction: %v", err.Error())
+		return nil, fmt.Errorf("submit-transaction: %v", err.Error())
 	}
-	return &rpcpb.SubmitTranstractionResponse{TxID: rawTx.ID}, nil
+	return &rpcpb.SubmitTransactionResponse{TxID: rawTx.ID}, nil
 }

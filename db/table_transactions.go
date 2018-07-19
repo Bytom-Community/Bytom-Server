da4fb6e3945dba5250a217f50376f0789431b672
coral <shanhu5739@gmail.com>
2018-07-19 11:19:11 +0800

    delete version field for db

--- db/table_transactions.go
+++ db/table_transactions.go
@@ -5,7 +5,6 @@ type (
 		TxId           string `xorm:"tx_id"`
 		BlockHash      string `xorm:"block_hash"`
 		Amount         int64  `xorm:"amount"`
-		Version        int64  `xorm:"version"`
 		SerializedSize int64  `xorm:"serialized_size"`
 		TimeRange      int64  `xorm:"timerange"`
 	}
